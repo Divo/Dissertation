9dab58fa1c55383d8e4d661b62863ef1a9dcb23d
Mr. Divenson <divoapple@gmail.com>
2012-11-11 18:04:40 +0000

    SYnc

--- Parallel patch-based texture synthesis.mm
+++ Parallel patch-based texture synthesis.mm
@@ -193,8 +193,7 @@
       Calculating cut
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1352477051703" ID="ID_1288276443" MODIFIED="1352477080267">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -307,8 +306,7 @@
       Seam cost
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1352490603579" ID="ID_594995995" MODIFIED="1352490919839">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -322,8 +320,7 @@
       Similar to the one used in graph cut, that seems to enough explanaiton for them
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1352491006567" ID="ID_1184938510" MODIFIED="1352491026465">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -334,8 +331,7 @@
       Allows existing erros in S to be calculated
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
@@ -356,8 +352,7 @@
       This leaves an energy function with 3 terms
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <node CREATED="1352491139012" ID="ID_108573332" MODIFIED="1352491160106">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -368,8 +363,7 @@
       Cost of horizontal transitions along C
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1352491199629" ID="ID_696460940" MODIFIED="1352491389869">
 <richcontent TYPE="NODE"><html>
@@ -384,8 +378,7 @@
       V has to sum up all of the vertical costs that may appear in one increment due to Jmax beong relaxed.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1352491416002" ID="ID_127467695" MODIFIED="1352491471729">
 <richcontent TYPE="NODE"><html>
@@ -400,8 +393,7 @@
       The errors on the left side of C and will be hidden by P, so they are subtracted.
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1352491549668" ID="ID_47628945" MODIFIED="1352491599023">
@@ -414,29 +406,12 @@
       All solutions for the energy function are precomputed to optimize the DP
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 <node CREATED="1352491602714" ID="ID_1470543149" MODIFIED="1352491701823" TEXT="Approximate cuts">
 <node CREATED="1352491820706" ID="ID_1119315528" MODIFIED="1352491830659" TEXT="Need to read prior work to understand this"/>
 <node COLOR="#cc0033" CREATED="1352491837967" ID="ID_1882642915" MODIFIED="1352491851010" TEXT="JSTS06"/>
-<node CREATED="1352491857531" ID="ID_995802785" MODIFIED="1352491977815">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      Jist of it is:
-    </p>
-    <p>
-      They backtrack all paths from bottom to top to find at least one path with the same abscissa, rather than runing the optimization for every start and end point
-    </p>
-  </body>
-</html>
-</richcontent>
-</node>
 <node CREATED="1352492013652" ID="ID_489153909" MODIFIED="1352492049508">
 <richcontent TYPE="NODE"><html>
   <head>
@@ -450,13 +425,295 @@
       Any cases where it does not are likely to be rejected later
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
 <node CREATED="1352492186357" ID="ID_21757293" MODIFIED="1352492200923" POSITION="right" TEXT="Feature alignment">
 <icon BUILTIN="full-5"/>
+<node CREATED="1352492574321" ID="ID_1862249199" MODIFIED="1352492607734" TEXT="Cost cut does not gaurentee good results, particularly if a texture contains alligned structural patterns"/>
+<node CREATED="1352493647849" ID="ID_1067500901" MODIFIED="1352493729869">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Small deformations applied&#160;to align feature along each side of C
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1352493747368" ID="ID_267608840" MODIFIED="1352493780591" TEXT="Colours along C are offset to match colours in E[S] on te ither side of C">
+<node CREATED="1352494234266" ID="ID_356914567" MODIFIED="1352494277001" TEXT="Cost of verticl transitions ignored, Jmax makes offsetting the colours on the vertical too hard"/>
+<node CREATED="1352494342026" ID="ID_911270727" MODIFIED="1352494509189">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Again, this is done using a cost function
+    </p>
+    <p>
+      The offset image, D, is obtained by minimizing this cost function.
+    </p>
+    <p>
+      The offset is limitde to &lt;= 2 (chosen empirically) to minimize deformation.
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1352494844830" ID="ID_16992865" MODIFIED="1352494902220">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Inital State
+    </p>
+    <p>
+      D must start and end at the same abscissa in T. Same approximation is used as in approximating the cuts
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1352493781326" ID="ID_275516475" MODIFIED="1352493818292" TEXT="The deformation is then smoothly propagated to the inside of P">
+<node CREATED="1352494952948" ID="ID_293765865" MODIFIED="1352494997947" TEXT="The deformation is interpolated inwards using MATHS"/>
+</node>
+</node>
+</node>
+<node CREATED="1352495022370" ID="ID_1446690047" MODIFIED="1352495034769" POSITION="left" TEXT="Patch Rejection">
+<icon BUILTIN="full-6"/>
+<node CREATED="1352495045182" ID="ID_206795199" MODIFIED="1352495231191">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      iff enegy of cut &lt;= 0 then the patch is accepted
+    </p>
+    <p>
+      (Recall that this really means the errors in the new cut are less than the errors in the result so far)
+    </p>
+    <p>
+      This is applied before feature alignment.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1352495279838" ID="ID_1118450605" MODIFIED="1352495957163">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Next is applied after feature alignment
+    </p>
+    <p>
+      Energy along C after offsetting + Total energy in P after deformation - Total existing energy left of C &lt;= 0
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1352496046313" ID="ID_116325794" MODIFIED="1352496057588" POSITION="right" TEXT="Implmentation details">
+<icon BUILTIN="full-7"/>
+<node CREATED="1352496105540" ID="ID_309375210" MODIFIED="1352496149377">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      CUDA used
+    </p>
+    <p>
+      One thread per patch, simply limited by the boudries of the patch
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1352496161335" ID="ID_612950555" MODIFIED="1352496217889">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Buffers for energy of cut optimization is reused for energy of cut after offset
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1352496243451" ID="ID_369997572" MODIFIED="1352496261801" TEXT="Texture buffers allocated based on patch size"/>
+<node CREATED="1352558381498" ID="ID_1989096966" MODIFIED="1352558396631" TEXT="All transition costs pre-computed prior to optimization">
+<node CREATED="1352559116071" ID="ID_1125552332" MODIFIED="1352559134405" TEXT="These are stored in order to speed up the optimization function"/>
+</node>
+<node COLOR="#ff0066" CREATED="1352559170710" ID="ID_1934629123" MODIFIED="1352559865334">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      DP is optimized with top-down -sub-solution pre-computation
+    </p>
+    <p>
+      Don't really get this bit
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1352559239532" ID="ID_1185218733" MODIFIED="1352559260767" TEXT="For each patch, cut wth lowest cost selected"/>
+<node CREATED="1352559300402" ID="ID_682411585" MODIFIED="1352559328407">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Each row depends only on the row before it so it's very trivially parallisable
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1352559602071" ID="ID_1442778995" MODIFIED="1352559633723" TEXT="Accumulation of DP has 3 limitations for single row prallelism">
+<node CREATED="1352559662768" ID="ID_203685653" MODIFIED="1352559699492" TEXT="Parallelizing a single row at a time produces too few threads"/>
+<node CREATED="1352559722220" ID="ID_184586203" MODIFIED="1352559730689" TEXT="After a row is complete a global sync is needed"/>
+<node CREATED="1352559754880" ID="ID_250682956" MODIFIED="1352559800612" TEXT="The sync suspends the computation in a column for a long time. Other computations fill-up the cache in the mean time destroying it&apos;s coherence"/>
+<node CREATED="1352559815005" ID="ID_1440507041" MODIFIED="1352560438064">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      These are addressed by processing many rows at once. This is done by subdividing H into blocks
+    </p>
+    <p>
+      Blocks must be padded to left, right and above to ensure coherent calculation. These calculations are wasted but it's worth it for the coherence gains
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1352560614637" ID="ID_1642038422" MODIFIED="1352560626163" TEXT="Block size determined empirically"/>
+<node CREATED="1352560639394" ID="ID_867880935" MODIFIED="1352560653576" TEXT="Same approach used for bottom-up backtracking, without padding"/>
+</node>
+</node>
+<node CREATED="1352560730366" ID="ID_1041695976" MODIFIED="1352560795883" TEXT="Optimization of energy function after deformation is the same, except V and Ev are set to 0 as vertical transition costs are ignored. Jmax is set to 1"/>
+</node>
+</node>
+<node CREATED="1352560906213" ID="ID_1131357918" MODIFIED="1352560916014" POSITION="left" TEXT="Results">
+<icon BUILTIN="full-8"/>
+<node CREATED="1352560924716" ID="ID_751738070" MODIFIED="1352560937391" TEXT="S is init with white noise texture coordinates"/>
+<node CREATED="1352560938513" ID="ID_1007957625" MODIFIED="1352560994748" TEXT="One thing to note, the method, will impressive requires a fair amount of &quot;emprically determined&quot; constants, 7 in all, 6 if radius is discounted"/>
+<node CREATED="1352561066387" ID="ID_1245751301" MODIFIED="1352561110693">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      R is the maximum POSSIBLE radius
+    </p>
+    <p>
+      Also synthesisr must be iterated a few times to ensure complete coverage
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1352491857531" ID="ID_995802785" MODIFIED="1352491977815">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Jist of it is:
+    </p>
+    <p>
+      They backtrack all paths from bottom to top to find at least one path with the same abscissa, rather than runing the optimization for every start and end point
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1352561133650" ID="ID_1846406131" MODIFIED="1352561206707">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Radius has the most effect on quality.
+    </p>
+    <p>
+      Too small and intresting features are not captured.
+    </p>
+    <p>
+      Too large and no varity is produced
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1352561275302" ID="ID_767664480" MODIFIED="1352561331711" TEXT="The radius (and Dmax) for that matter can &quot;automatically&quot; be found by gradually reducing them, both no method for detecting an ideal result is presented">
+<node CREATED="1352561333713" ID="ID_994928675" MODIFIED="1352561353702" TEXT="The nature of the examplar has a big effect on these values"/>
+</node>
+<node CREATED="1352561376767" ID="ID_1572820680" MODIFIED="1352561421784">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Texture completion
+    </p>
+    <p>
+      Can also be used for texture completion by assigning infinte cost to holes
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1352561423326" ID="ID_1939041863" MODIFIED="1352561535028">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Texture painting
+    </p>
+    <p>
+      By using multiple examlpars
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1352561927851" ID="ID_358330801" MODIFIED="1352561933008" TEXT="Drag and drop, see video"/>
+<node CREATED="1352562301709" ID="ID_252869793" MODIFIED="1352562429448">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      PERFORMANCE, pg 9
+    </p>
+    <p>
+      Slower but gives far better results
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
 </node>
 </node>
 </map>
